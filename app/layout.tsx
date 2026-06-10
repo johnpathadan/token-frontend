6252c8b037c7f70cd2d96e8384e8372a54c27703
johnpathadan <johnpathadan@gmail.com>
2026-06-10 17:53:30 +0530

    google analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,35 @@
-import './globals.css';
+import "./globals.css";
+import Script from "next/script";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
-export const metadata = { title: 'RWA Token Studio' };
+export const metadata = { title: "RWA Token Studio" };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en" className={cn("font-sans", geist.variable)}>
       <body className="bg-amber-100 min-h-screen text-slate-900">
         {children}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+  `}
+        </Script>
       </body>
     </html>
   );
